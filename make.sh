a7e83662d7f4b610b2c5274533e8bcf62c0f6ca8
tsoganov <sergei.tsoganov@ut.ee>
2024-10-28 16:38:47 +0200

    Updated make.sh

--- make.sh
+++ make.sh
@@ -3,9 +3,9 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx @redocly/cli build-docs openapi/openapi.yaml && \
-mv redoc-static.html index.html && \
-echo "Changed name from redoc-static.html to index.html" && \
+npx @redocly/cli build-docs openapi/openapi.yaml -o index.html && \
+# mv redoc-static.html index.html && \
+# echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
-# sed -i '7 i \ \ <link rel="icon" type="image/png" href="docs/favicon.png"/>' index.html && \
+awk 'NR==8{print "    <link rel=\"icon\" type=\"image/png\" href=\"docs/favicon.png\"/>"} 1' index.html > temp.html && mv temp.html index.html && \
 echo -e "\nDone!"
