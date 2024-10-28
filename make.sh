bacf8e780279b55cfae8176f10bbdb1b166efef0
tsoganov <sergei.tsoganov@ut.ee>
2024-10-28 16:09:23 +0200

    Updated make.sh

--- make.sh
+++ make.sh
@@ -7,5 +7,5 @@ npx @redocly/cli build-docs openapi/openapi.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
-sed -i '7 i \ \ <link rel="icon" type="image/png" href="docs/favicon.png"/>' index.html && \
+# sed -i '7 i \ \ <link rel="icon" type="image/png" href="docs/favicon.png"/>' index.html && \
 echo -e "\nDone!"
